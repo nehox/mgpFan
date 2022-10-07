90274b273cc9a16a5d7e2ff5faadb0896a770d9d
Julien FICERAI <julien.ficerai@gmail.com>
2022-10-07 09:50:51 +0200

    initial commit

---
+++ mgpFan/mgpFanApp.swift
@@ -0,0 +1,17 @@
+//
+//  mgpFanApp.swift
+//  mgpFan
+//
+//  Created by Julien Ficerai on 07/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct mgpFanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
